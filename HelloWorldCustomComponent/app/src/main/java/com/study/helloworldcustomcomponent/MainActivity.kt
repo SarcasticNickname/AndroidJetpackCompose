6425d692de28ccd6b74199d817f0756a3d9c594c
SarcasticNickname <78605109+SarcasticNickname@users.noreply.github.com>
2024-09-29 19:51:46 +0300

    Custom Jetpack Compose component

--- HelloWorldCustomComponent/app/src/main/java/com/study/helloworldcustomcomponent/MainActivity.kt
+++ HelloWorldCustomComponent/app/src/main/java/com/study/helloworldcustomcomponent/MainActivity.kt
@@ -1,16 +1,28 @@
-package com.study.helloworldsimplified
+package com.study.helloworldcustomcomponent
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(text = "Hello World from my second project!", fontSize = 28.sp)
+
         }
     }
+}
+
+@Composable
+fun Hello() {
+    Text(
+        text = "Hello World from my custom jetpack compose component",
+        style = TextStyle(
+            fontSize = 24.sp
+        )
+    )
 }
