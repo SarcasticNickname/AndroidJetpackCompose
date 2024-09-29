d89ae80f1fb21328de3cad6a8010d3c1eb0d23c5
SarcasticNickname <78605109+SarcasticNickname@users.noreply.github.com>
2024-09-29 19:55:44 +0300

    Minor change to setContent

--- HelloWorldCustomComponent/app/src/main/java/com/study/helloworldcustomcomponent/MainActivity.kt
+++ HelloWorldCustomComponent/app/src/main/java/com/study/helloworldcustomcomponent/MainActivity.kt
@@ -6,13 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            Hello()
         }
     }
 }
@@ -25,4 +26,10 @@ fun Hello() {
             fontSize = 24.sp
         )
     )
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun HelloPreview() {
+    Hello()
 }
