ffc72a6ab4015a899ce7e6103fd83ce9957afd20
SarcasticNickname <78605109+SarcasticNickname@users.noreply.github.com>
2024-09-29 21:47:02 +0300

    Variables and functions Jetpack Compose

--- VariablesJetpackCompose/app/src/main/java/com/study/variablesjetpackcompose/MainActivity.kt
+++ VariablesJetpackCompose/app/src/main/java/com/study/variablesjetpackcompose/MainActivity.kt
@@ -3,26 +3,42 @@ package com.study.variablesjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.study.variablesjetpackcompose.ui.theme.VariablesJetpackComposeTheme
+import androidx.compose.ui.unit.sp
 import java.util.Calendar
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            Text(text = getTime(), fontSize = 24.sp)
+
+            val getFullTime = {
+                val calendar = Calendar.getInstance()
+                val hours = calendar.get(Calendar.HOUR_OF_DAY)
+                val minutes = calendar.get(Calendar.MINUTE)
+                val seconds = calendar.get(Calendar.SECOND)
+                val milli = calendar.get(Calendar.MILLISECOND)
+                "$hours:$minutes:$seconds:$milli"
+            }
+
+            val hours = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+            val currTicks = listOf("First", "Second", "Third")
+            Column {
+                for (tick in currTicks) {
+                    if (hours < 20) {
+                        Text(text = tick + " " + getFullTime() /*getTime()*/, fontSize = 24.sp)
+                    } else {
+                        Text(text = tick + " " + getTime(), fontSize = 24.sp)
+                    }
+                }
+            }
         }
     }
 }
 
-fun getTime() : String {
+fun getTime(): String {
     val calendar = Calendar.getInstance()
     val hours = calendar.get(Calendar.HOUR_OF_DAY)
     val minutes = calendar.get(Calendar.MINUTE)
